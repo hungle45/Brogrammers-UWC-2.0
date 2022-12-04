6a6cad61d66750bc1478e0c392d3dab70dfa9623
hoanghuydo787 <hoanghuydo787@gmail.com>
2022-12-04 16:58:42 +0700

    update

--- client/src/components/trollersList/TrollersList.js
+++ client/src/components/trollersList/TrollersList.js
@@ -8,65 +8,27 @@ const TrollersList = () => {
   const [trollers, setTrollers] = useState(api.TrollerAPI.all());
   return (
     <>
-      <Navbar />
+      <Navbar></Navbar>
       <>
-        <div>
-          <NavLink to='/vehicles/trollers'>Controller</NavLink>
+        <div style={{margin: '10px',}}>
+          <NavLink to='/vehicles/trollers'>Trollers</NavLink>
           <NavLink to='/vehicles/trucks'>Collect Vehicle</NavLink>
         </div>
-        <div>{!trollers.length ? <></> :
-          trollers.map((value, index) => {
-            return (<div key={value.id}>
-              <img src="" alt="Troller img"></img>
-              <div>{value.id}</div>
-              <div>{!value.status ? "Available" : "Unavailable"}</div>
-            </div>)
-          })}</div>
-        
-        <div style={{margin: '10px',}}>
-            <button> MCP controllers</button>
-            <button> Collecting vehicles</button>
-        </div>
 
-        <div class="controller-container">
-            <div class="controller-container-column">
+
+        <div class="controller-container">{!trollers.length ? <></> :
+          trollers.map((value, index) => {
+            return (
+            <div key={value.id} class="controller-container-column">
                 <div class="controller-container-small-column">
-                    <img src="./images/mcp-controller.png" alt='sth'></img>
+                    <img src="./images/mcp-controller.png" alt='troller img'></img>
                 </div>
                 <div class="controller-container-small-column">
-                    <label> 78A1-8501</label><br></br>
-                    <label> Available</label>
-                </div>
-            </div>
-            <div class="controller-container-column">
-                <div class="controller-container-small-column">
-                    <img src="./images/mcp-controller.png" alt='sth'></img>
-                </div>
-                <div class="controller-container-small-column">
-                    <label> 78A1-8501</label><br></br>
-                    <label> Available</label>
+                    <div>{value.id}</div>
+                    <div>{!value.status ? "Available" : "Unavailable"}</div>
                 </div>
             </div>
-        </div>
-        <div class="controller-container">
-            <div class="controller-container-column">
-                <div class="controller-container-small-column">
-                    <img src="./images/mcp-controller.png" alt='sth'></img>
-                </div>
-                <div class="controller-container-small-column">
-                    <label> 78A1-8501</label><br></br>
-                    <label> Available</label>
-                </div>
-            </div>
-            <div class="controller-container-column">
-                <div class="controller-container-small-column">
-                    <img src="./images/mcp-controller.png" alt='sth'></img>
-                </div>
-                <div class="controller-container-small-column">
-                    <label> 78A1-8501</label><br></br>
-                    <label> Available</label>
-                </div>
-            </div>
+            )})}
         </div>
       </>
     </>
--- client/src/components/trucksList/TrucksList.js
+++ client/src/components/trucksList/TrucksList.js
@@ -2,25 +2,35 @@ import React, { useState } from 'react'
 import Navbar from '../navbar/Navbar'
 import api from '../../model/api/api';
 import { NavLink } from "react-router-dom";
+import './trucksList.css'
 
 const TrucksList = () => {
   const [trucks, setTrucks] = useState(api.CollectingVehicleAPI.all());
   return (
     <>
-      <Navbar />
+      <Navbar></Navbar>
       <>
-        <div>
-          <NavLink to='/vehicles/trollers'>Controller</NavLink>
+        <div style={{margin: '10px',}}>
+          <NavLink to='/vehicles/trollers'>Trollers</NavLink>
           <NavLink to='/vehicles/trucks'>Collect Vehicle</NavLink>
         </div>
-        <div>{!trucks.length ? <></> :
+
+
+        <div class="controller-container">{!trucks.length ? <></> :
           trucks.map((value, index) => {
-            return (<div key={value.id}>
-              <img src="" alt="Truck img"></img>
-              <div>{value.id}</div>
-              <div>{!value.status ? "Available" : "Unavailable"}</div>
-            </div>)
-          })}</div>
+            return (
+            <div key={value.id} class="controller-container-column">
+              <div class="controller-container-small-column">
+                <img src="./images/mcp-controller.png" alt="Truck img"></img>
+              </div>
+
+              <div class="controller-container-small-column">
+                <div>{value.id}</div>
+                <div>{!value.status ? "Available" : "Unavailable"}</div>
+              </div>
+            </div>
+            )})}
+        </div>
       </>
     </>
 
